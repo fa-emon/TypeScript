5dc4204bb86d2dd2ebc2393ef742d7896f617632
Farhan933 <farhanfarabiemon@gmail.com>
2024-03-13 14:09:03 +0600

    union-type

---
+++ user-defined-type/union-type.js
@@ -0,0 +1,9 @@
+var userId;
+userId = '10';
+userId = 10;
+userId = true;
+function displayInfo(userId) {
+    console.log(userId);
+}
+displayInfo(101);
+displayInfo('101');
---
+++ user-defined-type/union-type.ts
@@ -0,0 +1,11 @@
+let userId: (string | number | boolean)
+
+userId = '10'
+userId = 10
+userId = true
+
+function displayInfo(userId: string | number){
+    console.log(userId);
+}
+displayInfo(101)
+displayInfo('101')
