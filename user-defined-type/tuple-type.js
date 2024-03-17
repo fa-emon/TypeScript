20bb69ddc685cdd208a730b015ffbdaa4fd77c54
Farhan933 <farhanfarabiemon@gmail.com>
2024-03-17 23:54:16 +0600

    tuple-type

---
+++ user-defined-type/tuple-type.js
@@ -0,0 +1,4 @@
+// mixed data : key value 
+var user;
+user = [10, 'film'];
+console.log(user);
---
+++ user-defined-type/tuple-type.ts
@@ -0,0 +1,6 @@
+// mixed data : key value 
+
+let user : [number, string];
+    user = [10, 'film'];
+
+    console.log(user);
